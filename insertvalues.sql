80b7fff232e3de56c92a853b5deee85718ef1a9f
Gannu <vineel.gannu@cgi.com>
2022-03-22 11:17:20 -0600

    updated insertvalues sql file

--- insertvalues.sql
+++ insertvalues.sql
@@ -1,4 +1,4 @@
-insert into students (StudentId, Name, Email) VALUES (1, 'Vineel Gannu', 'Vineel@gmail.com');
-insert into students (StudentId, Name, Email) VALUES (2, 'John Thompson', 'John@gmail.com');
-insert into students (StudentId, Name, Email) VALUES (3, 'Peter Smith', 'Peter@gmail.com');
-insert into students (StudentId, Name, Email) VALUES (4, 'James Cameron', 'James@gmail.com');
+insert into student (StudentId, Name, Email) VALUES (1, 'Vineel Gannu', 'Vineel@gmail.com');
+insert into student (StudentId, Name, Email) VALUES (2, 'John Thompson', 'John@gmail.com');
+insert into student (StudentId, Name, Email) VALUES (3, 'Peter Smith', 'Peter@gmail.com');
+insert into student (StudentId, Name, Email) VALUES (4, 'James Cameron', 'James@gmail.com');
